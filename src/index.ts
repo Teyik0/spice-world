b54457d6568d837d43a78fceb1e156a7a03c58d4
teyik0 <teyik0.fr@icloud.com>
2024-11-24 17:29:55 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
 
+const betterAuthView = (context: Context) => {
+  const BETTER_AUTH_ACCEPT_METHODS = ["POST", "GET"];
+  // validate request method
+  if (BETTER_AUTH_ACCEPT_METHODS.includes(context.request.method)) {
+    return auth.handler(context.request);
+  } else {
+    context.error(405);
+  }
+};
+
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
