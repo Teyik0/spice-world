63cfe72617bd4d4ceb1418e00392e786874e92a2
teyik0 <teyik0.fr@icloud.com>
2025-04-30 08:05:31 +0200

    dev.ts spawn front, server and email in once

--- dev.ts
+++ dev.ts
@@ -1,37 +1,29 @@
-import type { SpawnOptions } from "bun";
+import type { SpawnOptions } from 'bun'
 
 const spawnOptions: SpawnOptions.OptionsObject = {
-  stdin: "inherit",
-  stdout: "inherit",
-  stderr: "inherit",
+  stdin: 'inherit',
+  stdout: 'inherit',
+  stderr: 'inherit',
 }
 
 const run = async () => {
   // Run all scripts in parallel
   const processes = [
-    Bun.spawn(
-      ["bun", "run", "dev"],
-      {...spawnOptions, cwd: "./apps/server"}
-    ),
-    Bun.spawn(
-      ["bun", "run", "dev"],
-      {...spawnOptions, cwd: "./apps/dashboard"}
-    ),
-    Bun.spawn(
-      ["bun", "run", "dev"],
-      {...spawnOptions, cwd: "./packages/emails"}
-    ),
-  ];
+    Bun.spawn(['bun', 'run', 'dev'], { ...spawnOptions, cwd: './apps/server' }),
+    Bun.spawn(['bun', 'run', 'prisma', 'studio'], { ...spawnOptions, cwd: './apps/server' }),
+    Bun.spawn(['bun', 'run', 'dev'], { ...spawnOptions, cwd: './apps/dashboard' }),
+    Bun.spawn(['bun', 'run', 'dev'], { ...spawnOptions, cwd: './packages/emails' }),
+  ]
 
   // Handle cleanup on SIGINT
-  process.on("SIGINT", async () => {
-    console.log("Cleaning up...");
-    processes.forEach((process) => process.kill());
-    process.exit(0);
-  });
+  process.on('SIGINT', async () => {
+    console.log('Cleaning up...')
+    processes.forEach((process) => process.kill())
+    process.exit(0)
+  })
 
   // Wait for all processes to finish (if needed)
-  await Promise.all(processes.map((process) => process.exited));
-};
+  await Promise.all(processes.map((process) => process.exited))
+}
 
-run();
+run()
